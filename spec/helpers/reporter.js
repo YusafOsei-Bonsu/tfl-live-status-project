ad3c5408343d4aa9b96bf2c8e44b4c4887155c96
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-02-28 18:42:19 +0000

    Shows which tests have passed/failed in a better way

---
+++ spec/helpers/reporter.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
+// remove default reporter logs
+jasmine.getEnv().clearReporters();  
+
+// add jasmine-spec-reporter
+jasmine.getEnv().addReporter(new SpecReporter({  
+  spec: {
+    displayPending: true
+  }
+}));

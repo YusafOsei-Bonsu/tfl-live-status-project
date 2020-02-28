0009dcc5459d7952657850e14871aa86152034b7
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-02-28 19:10:41 +0000

    Shows hello world as a h1 tag

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
   }
 }
-
-export default App;

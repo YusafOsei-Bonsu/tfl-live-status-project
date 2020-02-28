9a69c41874176d1e71ea772c69a0f9cc83af721b
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-02-28 20:19:01 +0000

    A component that shows a tube map of central

---
+++ src/components/CentralTubeMap.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CentralTubeMap = () => {
+    return <img src="assets/central-tube-map.jpg" alt="A tube map of central London" />
+}
+
+export default CentralTubeMap;

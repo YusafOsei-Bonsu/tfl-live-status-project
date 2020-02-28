6b61d84a0a30e04482fc31bc52331c066bf76684
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-02-28 21:02:16 +0000

    Completed component that shows Tfl map of Central London (#2)
    * Deleted unneccessary files
    
    * A component that shows a tube map of central
    
    * Removed unneccessary LOC
    
    * Central tube map (tfl) and its styling
    
    * Padding around the body
    
    * Flex-container and h1 styling
    
    * Moved styling into centralmaptube.css

---
+++ src/components/CentralTubeMap.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/CentralTubeMap.css";
+
+// An image of the Tfl tube map of central
+const CentralTubeMap = () => {
+    return (
+        <div id="map">
+            <img src="assets/central-tube-map.jpg" alt="A tube map of central London" />
+        </div>
+    );
+}
+
+export default CentralTubeMap;

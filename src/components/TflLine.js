aea107114da464f9fcf0d3c5bbd4ae63b083056f
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-03-01 18:32:39 +0000

    Update TflLine.js

--- src/components/TflLine.js
+++ src/components/TflLine.js
@@ -7,11 +7,11 @@ class TflLine extends React.Component {
         return (
             // Each Tfl line is represented by a BS4 card
             <div class="card">
-                <div class="card-header" style={color}><h3>{ name }</h3></div>
+                <div class="card-header" style={ color }><h3>{ name }</h3></div>
                 <div class="card-body">
                     <h5>{ status }</h5>
                     {/* If the status reason exists, display it */}
-                    {reason ? <p>{reason}</p> : <p>{name} Line is experiencing a good service today. Have a nice day :)</p>}
+                    {reason ? <p>{ reason }</p> : <p>{ name } Line is experiencing a good service today. Have a nice day :)</p>}
                 </div> 
                 <div class="card-footer"><br /></div>
             </div>

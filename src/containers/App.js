33ee57afe8a7bb8e0f5d2e98ba9b57240e6b2be7
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-03-01 18:03:31 +0000

    Shows each Tfl service line and their status  (#6)
    * Deleted CentralTubeMap component
    
    * Removed redundant styling
    
    * Installed Axios and Dotenv package
    
    * Ignoring sensitive information
    
    * Installed dotenv package
    
    * Project brief and blockers/resolutions
    
    * Update App.js
    
    * Imported Bootstrap to use BS4 cards
    
    * Component for each Tfl line and its styling
    
    * Updated App.js and its styling
    
    * Adding spacing between header and tfl lines

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import CentralTubeMap from '../components/CentralTubeMap';
 import TflHeader from '../components/TflHeader';
-import '../styles/App.css'
+import TflLine from '../components/TflLine';
+import Axios from 'axios';
+import '../styles/App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Stores each tfl line
+      tflLines: [],
+      // Distinct colour of each line
+      tflLineColour: { bakerloo: '#a52a2a', central: '#ff0000', circle: '#ffff00', district: '#008000', dlr: '#20B2AA', "hammersmith-city": "#DB7093", jubilee: "#A9A9A9",
+        "london-overground": "#FF8C00", metropolitan: "#800080", northern: "#000000", piccadilly: "#00008B", tram: "#7FFF00", victoria: "#00CED1", "waterloo-city": "#87CEFA"
+      }
+    }
+  }
+
+  // Getting all Tfl service lines and their line statuses
+  componentDidMount() {
+    const appId = process.env.REACT_APP_ID;
+    const appKey = process.env.REACT_APP_KEY;
+    Axios.get(`https://api.tfl.gov.uk/Line/Mode/tube%2Cdlr%2Coverground%2Ctram/Status?app_id=${appId}&app_key=${appKey}`)
+      .then(res => {
+        this.setState({ tflLines: res.data });
+      }).catch(err => console.log(err));
+  }
+
   render() {
+    // Deconstructing state
+    let { tflLines, tflLineColour } = this.state;
+
     return (
       <div>
         <TflHeader />
+        <div className="flex-container">
+        {tflLines.map(line => {
+          // Color of tfl service line
+          let color = { backgroundColor: tflLineColour[line.id] }
+          return (
+            <TflLine 
+              name={line.name} 
+              color={color} 
+              status={line.lineStatuses[0]["statusSeverityDescription"]}
+              reason={line.lineStatuses[0]["reason"]} />
+            );
+        })}
+        </div>
       </div>
     );
   }

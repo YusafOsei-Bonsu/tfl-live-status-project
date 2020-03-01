411e93d1b2261cb28042681610b69d7ff42a99bc
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-03-01 17:58:30 +0000

    Updated App.js and its styling

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TflHeader from '../components/TflHeader';
+import TflLine from '../components/TflLine';
 import Axios from 'axios';
 import '../styles/App.css';
 
@@ -7,7 +8,12 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tflLines: []
+      // Stores each tfl line
+      tflLines: [],
+      // Distinct colour of each line
+      tflLineColour: { bakerloo: '#a52a2a', central: '#ff0000', circle: '#ffff00', district: '#008000', dlr: '#20B2AA', "hammersmith-city": "#DB7093", jubilee: "#A9A9A9",
+        "london-overground": "#FF8C00", metropolitan: "#800080", northern: "#000000", piccadilly: "#00008B", tram: "#7FFF00", victoria: "#00CED1", "waterloo-city": "#87CEFA"
+      }
     }
   }
 
@@ -22,9 +28,25 @@ class App extends React.Component {
   }
 
   render() {
+    // Deconstructing state
+    let { tflLines, tflLineColour } = this.state;
+
     return (
       <div>
         <TflHeader />
+        <div className="flex-container">
+        {tflLines.map(line => {
+          // Color of tfl service line
+          let color = { backgroundColor: tflLineColour[line.id] }
+          return (
+            <TflLine 
+              name={line.name} 
+              color={color} 
+              status={line.lineStatuses[0]["statusSeverityDescription"]}
+              reason={line.lineStatuses[0]["reason"]} />
+            );
+        })}
+        </div>
       </div>
     );
   }

52d948d5d0d330cb7c0aa628e939b7afe454c156
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-03-03 21:35:27 +0000

    Shows the update time in hours and minutes (#7)
    * Update TflLine.js
    
    * Cards are shown in a layered fashion
    
    * Timestamp of last status update is shown in footer
    
    * Increasing width of BS4 cards
    
    * Changed flexbox styling
    
    * Reduced width of BS4 cards
    
    * Displays the status reason if one exists
    
    * Injecting the Tfl logo into the header
    
    * Each colour takes its own line
    
    * New line in the footer
    
    * Condensed colours to a few lines
    
    * Making the central london tfl map the background of the webpage
    
    * Provided each instance with a timestamp of live status
    
    * Showing the timestamp

--- src/components/TflLine.js
+++ src/components/TflLine.js
@@ -2,18 +2,20 @@ import React from 'react';
 import "../styles/TflLine.css";
 
 class TflLine extends React.Component {
+
     render() {
-        let { name, color, status, reason } = this.props;
+        let { name, color, status, reason, timestamp } = this.props;
+        
         return (
             // Each Tfl line is represented by a BS4 card
-            <div class="card">
-                <div class="card-header" style={color}><h3>{ name }</h3></div>
-                <div class="card-body">
+            <div className="card">
+                <div className="card-header" style={ color }><h3>{ name }</h3></div>
+                <div className="card-body">
                     <h5>{ status }</h5>
                     {/* If the status reason exists, display it */}
-                    {reason ? <p>{reason}</p> : <p>{name} Line is experiencing a good service today. Have a nice day :)</p>}
+                    {reason && <p>{ reason }</p>}
                 </div> 
-                <div class="card-footer"><br /></div>
+                <div className="card-footer">{ timestamp ? <h6>Last Updated: {(new Date(timestamp.fromDate).getHours()).toString().padStart(2, '0')}:{(new Date(timestamp.fromDate).getMinutes()).toString().padStart(2, '0')}</h6> : <br />}</div>
             </div>
         );
     }
---
+++ src/components/TflLogo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TflLogo = () => {
+    return <img src="assets/tfl_logo.png" alt="tfl logo" />
+}
+
+export default TflLogo;

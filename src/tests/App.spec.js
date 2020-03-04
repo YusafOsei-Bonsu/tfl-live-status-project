70c9d6dddbb8734c0b0da7de48207ece7071f26c
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-03-04 20:34:54 +0000

    Initial tests to render the two divs and TflHeader instance

--- src/tests/App.spec.js
+++ src/tests/App.spec.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render, mount } from 'enzyme';
 import App from '../containers/App';
+import TflHeader from '../components/TflHeader';
+import TflLine from '../components/TflLine';
 
 describe('App', () => {
+  let wrapper;
 
-  it('Should render 2 divs', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('div').length).toEqual(2);
-  })
+  // Tests if the main and flex-container div are being rendered
+  test('Should render 2 divs', () => {
+    wrapper = shallow(<App />);
+    expect(wrapper.find('div').length).toEqual(2)
+});
+
+  // Tests if the TflHeader instance gets rendered
+  test('should render the TflHeader instance', () => {
+    wrapper = shallow(<App />);
+    expect(wrapper.containsMatchingElement(<TflHeader />)).toBeTruthy();
+  });
+  
 });

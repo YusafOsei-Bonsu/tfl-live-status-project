72c0c5ed263aca701d0cee87864192b82698e9a9
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-03-04 15:08:11 +0000

    Ready to be deployed on Heroku

---
+++ build/precache-manifest.d7b2bec35361e4223be9c03e8f52341e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6042e67649fb4f5c886c9de40f2a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1142fb8fd645dcd4223b",
+    "url": "/static/css/main.86263cee.chunk.css"
+  },
+  {
+    "revision": "32095125f280cab1f23e",
+    "url": "/static/js/2.300c4188.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.300c4188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1142fb8fd645dcd4223b",
+    "url": "/static/js/main.4dc91434.chunk.js"
+  },
+  {
+    "revision": "0610c457c4058f886e42",
+    "url": "/static/js/runtime-main.5493ded9.js"
+  },
+  {
+    "revision": "5abfdace09324001087ac81fbbb95106",
+    "url": "/static/media/tube-map.5abfdace.jpg"
+  }
+]);

3ab372afbb3132793a7fac6b48f9c7f7b6f1e962
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-03-04 21:11:33 +0000

    API call test

--- src/tests/App.spec.js
+++ src/tests/App.spec.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { shallow, render, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from '../containers/App';
 import TflHeader from '../components/TflHeader';
-import TflLine from '../components/TflLine';
+import Axios from 'axios';
 
 describe('App', () => {
   let wrapper;
@@ -18,6 +18,16 @@ describe('App', () => {
   test('should render the TflHeader instance', () => {
     wrapper = shallow(<App />);
     expect(wrapper.containsMatchingElement(<TflHeader />)).toBeTruthy();
+  });
+  
+  // Tests if the API call gets data about all Tfl lines
+  test('should return data about all Tfl lines', () => {
+    const appId = process.env.REACT_APP_ID;
+    const appKey = process.env.REACT_APP_KEY;
+    Axios.get(`https://api.tfl.gov.uk/Line/Mode/tube%2Cdlr%2Coverground%2Ctram/Status?app_id=${appId}&app_key=${appKey}`)
+    .then(res => {
+      expect((res.data).length).toEqual(14);
+    }).catch(err => console.log(err));
   });
   
 });
